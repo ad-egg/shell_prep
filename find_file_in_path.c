1259bf28a8315cc4400450139707bdbda6120669
Athena Deng <539@holbertonschool.com>
2018-11-13 04:58:35 +0000

    incomplete work

---
+++ find_file_in_path.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+/**
+ * main - looks for files in the current PATH
+ * @ac: number of arguments
+ * @av: pointers to the arguments
+ * Return: 0 on success
+ */
+int main(int ac, char **av)
+{
+	unsigned int i;
+	struct stat now;
+
+	if (ac == 1)
+		return (1);
+	for (i = 0; av[i] != NULL; i++)
+	{
+		/* print the argument? */
+		if (stat(av[i], &now) == 0)
+			/* do something if find the file */
+			;
+		else
+			/* do something else if don't find file */
+			;
+	}
+	return (0);
+}
