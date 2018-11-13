41d49cf9304ed7dd9ecf3242a465404f7ff9fa16
Athena Deng <539@holbertonschool.com>
2018-11-13 05:12:19 +0000

    first commit

---
+++ 0-printenv_w_environ.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * main - prints the environment using the global variable environ
+ * @ac: number of arguments
+ * @av: pointers to the arguments
+ * @env: NULL terminated array of strings
+ * Return: 0 on success
+ */
+int main(int ac, char **av, char **env)
+{
+	int i;
+
+	for (i = 0; env[i] != NULL; i++)
+	{
+		printf("%s\n", env[i]);
+	}
+	return (0);
+}
